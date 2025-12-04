c3dd03dfd879914537832cf440e419ae5df88938
AdepuShashank <shahsank.adepu5@gmail.com>
2025-12-04 19:51:15 +0530

    test CI-4

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 func homePage(w http.ResponseWriter, r *http.Request) {
 	// Render the home html page from static folder
 	http.ServeFile(w, r, "static/home.html")
-	
+
 }
 
 func coursePage(w http.ResponseWriter, r *http.Request) {
